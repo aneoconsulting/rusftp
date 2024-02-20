429e800f3e5679626b8ea6f18e30dfbf9ced4a74
Florian Lemaitre <flemaitre@aneo.fr>
2024-02-20 15:30:59 +0100

    v0.1.0

---
+++ src/error.rs
@@ -0,0 +1,66 @@
+// This file is part of the rusftp project
+//
+// Copyright (C) ANEO, 2024-2024. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#[derive(Debug)]
+pub enum Error {
+    NotEnoughData,
+    Unsupported,
+    InvalidChar,
+    Custom(String),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::NotEnoughData => f.write_str("Decode Error: Not enough data"),
+            Error::Unsupported => f.write_str("Decode Error: Unsupported"),
+            Error::InvalidChar => f.write_str("Decode Error: Invalid character"),
+            Error::Custom(msg) => write!(f, "Decode Error: {msg}"),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+
+    fn description(&self) -> &str {
+        "description() is deprecated; use Display"
+    }
+
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        self.source()
+    }
+}
+
+impl serde::de::Error for Error {
+    fn custom<T>(msg: T) -> Self
+    where
+        T: std::fmt::Display,
+    {
+        Self::Custom(msg.to_string())
+    }
+}
+
+impl serde::ser::Error for Error {
+    fn custom<T>(msg: T) -> Self
+    where
+        T: std::fmt::Display,
+    {
+        Self::Custom(msg.to_string())
+    }
+}
