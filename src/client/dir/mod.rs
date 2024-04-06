52d7dc66b32539632ce8f2c7837a9a990b1a2bd0
Florian Lemaitre <flemaitre@aneo.fr>
2024-04-06 17:55:50 +0200

    Split dir module

--- src/client/dir/mod.rs
+++ src/client/dir/mod.rs
@@ -14,11 +14,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::{future::Future, pin::Pin, task::ready};
+use std::{future::Future, pin::Pin};
 
-use futures::Stream;
+use crate::{ClientError, Close, Handle, Name, SftpClient};
 
-use crate::{ClientError, Close, Handle, Name, NameEntry, ReadDir, SftpClient, Status, StatusCode};
+mod stream;
 
 /// Directory accessible remotely with SFTP
 pub struct Dir {
@@ -109,74 +109,5 @@ impl std::fmt::Debug for Dir {
             .field("buffer", &self.buffer)
             .field("pending", &self.pending.as_ref().map(|_| "..."))
             .finish()
-    }
-}
-
-impl Stream for Dir {
-    type Item = Result<NameEntry, ClientError>;
-
-    fn poll_next(
-        mut self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Option<Self::Item>> {
-        // If end of file reached, resturn None
-        let Some(buffer) = &mut self.buffer else {
-            return std::task::Poll::Ready(None);
-        };
-
-        // If still some entries in the buffer, get next
-        if let Some(entry) = buffer.0.pop() {
-            return std::task::Poll::Ready(Some(Ok(entry)));
-        }
-
-        let result = match &mut self.pending {
-            Some(pending) => ready!(pending.as_mut().poll(cx)),
-            None => {
-                let Some(handle) = &self.handle else {
-                    // Force end of iteration
-                    self.buffer = None;
-                    return std::task::Poll::Ready(Some(Err(std::io::Error::new(
-                        std::io::ErrorKind::BrokenPipe,
-                        "Dir was closed",
-                    )
-                    .into())));
-                };
-
-                let readdir = self.client.request(ReadDir {
-                    handle: handle.clone(),
-                });
-                let pending = self.pending.insert(Box::pin(readdir));
-
-                ready!(pending.as_mut().poll(cx))
-            }
-        };
-
-        // If the read was successful, the buffer will be populated again
-        // Stop the iteration otherwise
-        self.buffer = None;
-
-        let result = match result {
-            Ok(mut entries) => {
-                entries.reverse();
-
-                if let Some(entry) = entries.0.pop() {
-                    self.buffer = Some(entries);
-                    Some(Ok(entry))
-                } else {
-                    Some(Err(std::io::Error::new(
-                        std::io::ErrorKind::UnexpectedEof,
-                        "Found no more directory entries while it was expecting some",
-                    )
-                    .into()))
-                }
-            }
-            Err(ClientError::Sftp(Status {
-                code: StatusCode::Eof,
-                ..
-            })) => None,
-            Err(err) => Some(Err(err)),
-        };
-
-        std::task::Poll::Ready(result)
     }
 }
